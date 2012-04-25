a2c06a42296e4ec431f998ca8e2759a44bcf420c
eryx <evorui@gmail.com>
2012-04-25 23:59:32 +0800

    1.0.3.beta, APIs updated

--- src/config.h
+++ src/config.h
@@ -17,32 +17,32 @@
 #include <math.h>
 
 
-#define HCS_SIGNATURE   "hcaptcha/1.0.1.beta"
+#define HCS_SIGNATURE   "hcaptcha/1.0.3.beta"
 #define CONFIGLINE_MAX  1024
 
 struct configObject {
-  int   port;
-  int   daemon;
-  int   http_timeout;
-  char  *symbols;
-  char  *servers;
-  int   img_width;
-  int   img_height;
-  int   len;
-  
-  char   *font;
-  double  font_size;
-  
-  int   fluctuation_amplitude;
-  char  *log;
-  
-  int   img_timeout;
-  int   img_bg_color[3];
-  int   img_fg_color[3];
-  
-  int   length[2];
-  
-  char  *pidfile;
+    int   port;
+    int   daemon;
+    int   http_timeout;
+    char  *symbols;
+    char  *servers;
+    int   img_width;
+    int   img_height;
+    int   len;
+    
+    char   *font;
+    double  font_size;
+    
+    int   fluctuation_amplitude;
+    char  *log;
+    
+    int   img_timeout;
+    int   img_bg_color[3];
+    int   img_fg_color[3];
+    
+    int   length[2];
+    
+    char  *pidfile;
 } cfg;
 
 void initConfig();
